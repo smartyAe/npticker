a2e8ad0831332033918f2c345020e35c95b8eff9
Coulibaly Athanase <coulibalyathanase@gmail.com>
2025-01-30 02:52:17 +0100

    MAJ, ajout routage et header de l'app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import { BrowserRouter,Routes,Route,Link } from 'react-router-dom';
+import { ROUTES } from './Environement/APP_CONSTANTS';
+import Home from './Components/Home/Home';
+import Services from './Components/Services/Services';
+import Contact from './Components/Contact/Contact';
+import Projects from './Components/Projects/Projects';
+import Blog from './Components/Blog/Blog';
+import { useEffect, useState } from 'react';
+
 
 function App() {
+
+  const [CurrentPage,setCurrentPage] = useState(ROUTES.home);
+
+  useEffect(()=>{
+    const path=window.location.pathname;
+    setCurrentPage(path);
+  },[]);
+  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+            <nav className='ntk-navbar'>
+                <ul className='ntk-nav'>
+                  <li className='ntk-nav-item'>
+                    <Link className=
+                      {
+                        CurrentPage==ROUTES.home
+                        ? 'ntk-nav-link active'
+                        : 'ntk-nav-link'
+                      }
+                       to={ROUTES.home}
+                       onClick={(e)=>setCurrentPage(ROUTES.home)}
+                       >Home</Link>
+                  </li>
+                  <li className='ntk-nav-item'>
+                    <Link className=
+                    {
+                      CurrentPage==ROUTES.services
+                      ? 'ntk-nav-link active'
+                      : 'ntk-nav-link'
+                    }
+                     onClick={(e)=>setCurrentPage(ROUTES.services)}
+                      to={ROUTES.services}
+                      >Services</Link>
+                  </li>
+                  <li className='ntk-nav-item'>
+                    <Link className=
+                    {
+                      CurrentPage==ROUTES.projects
+                      ? 'ntk-nav-link active'
+                      : 'ntk-nav-link'
+                    }
+                     onClick={(e)=>setCurrentPage(ROUTES.projects)} to={ROUTES.projects}>Projects</Link>
+                  </li>
+                  <li className='ntk-nav-item'>
+                    <Link className=
+                    {
+                      CurrentPage==ROUTES.blog
+                      ? 'ntk-nav-link active'
+                      : 'ntk-nav-link'
+                    }
+                     onClick={(e)=>setCurrentPage(ROUTES.blog)}
+                     to={ROUTES.blog}>Blog</Link>
+                  </li>
+                  <li className='ntk-nav-item'>
+                    <Link className=
+                    {
+                      CurrentPage==ROUTES.contact
+                      ? 'ntk-nav-link active'
+                      : 'ntk-nav-link'
+                    }
+                     onClick={(e)=>setCurrentPage(ROUTES.contact)}
+                    to={ROUTES.contact}>Contact</Link>
+                  </li>
+                </ul>
+            </nav>
+            <Routes>
+                <Route path={ROUTES.home} element={<Home/>}></Route>
+                <Route path={ROUTES.services} element={<Services/>}></Route>
+                <Route path={ROUTES.projects} element={<Projects/>}></Route>
+                <Route path={ROUTES.blog} element={<Blog/>}></Route>
+                <Route path={ROUTES.contact} element={<Contact/>}></Route>
+            </Routes>
+      </BrowserRouter>
   );
 }
 
